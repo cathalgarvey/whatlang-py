da719269029225d339da0e795d10f0a080d8ad6f
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2017-04-19 09:34:41 +0100

    Adding wheel to Github, because PyPI is broken :(

--- setup.py
+++ setup.py
@@ -2,14 +2,26 @@ from setuptools import setup, find_packages
 from setuptools_rust import RustExtension
 
 setup(name='whatlang',
-      version='0.0.1',
-      description="Simple python bindings to Sergey Potapov's whatlang-rs",
-      long_description="This just wraps github.com/greyblake/whatlang-rs for Python 3.",
-      author="Cathal Garvey",
-      author_email="cathalgarvey@cathalgarvey.me",
-      url='https://github.com/cathalgarvey/whatlang-py',
-      license='MIT',
-      packages=["whatlang"],
-      rust_extensions=[RustExtension('whatlang.whatlang', 'whatlang-pybindings/Cargo.toml')],
-      zip_safe=False
+    version='0.0.1',
+    description="Simple python bindings to Sergey Potapov's whatlang-rs",
+    long_description="This just wraps github.com/greyblake/whatlang-rs for Python 3.",
+    author="Cathal Garvey",
+    author_email="cathalgarvey@cathalgarvey.me",
+    url='https://github.com/cathalgarvey/whatlang-py',
+    license='MIT',
+    packages=["whatlang"],
+    rust_extensions=[RustExtension('whatlang.whatlang', 'whatlang-pybindings/Cargo.toml')],
+    zip_safe=False,
+    keywords='language recognition heuristic nlp',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Programming Language :: Rust',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
+    ]
 )
